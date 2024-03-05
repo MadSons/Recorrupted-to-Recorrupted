ce86a72e27a49e1dfcfe9c6934a595ce2f89df78
Sean Chang <mine97059@gmail.com>
2024-03-05 13:16:52 -0500

    removed unnecessary

--- utils.py
+++ utils.py
@@ -26,38 +26,13 @@ def batch_PSNR(img, imclean, data_range):
 
 def data_augmentation(image, mode):
     out = np.transpose(image, (1,2,0))
-    if mode == 0:
-        # original
+    if mode == 0:# original
         out = out
-    elif mode == 1:
-        # flip up and down
-        out = np.flipud(out)
-    elif mode == 2:
-        # rotate counterwise 90 degree
+    elif mode == 1: # rotate 90 degree
         out = np.rot90(out)
-    elif mode == 3:
-        # rotate 90 degree and flip up and down
-        out = np.rot90(out)
-        out = np.flipud(out)
-    elif mode == 4:
-        # rotate 180 degree
+    elif mode == 2: # rotate 180 degree
         out = np.rot90(out, k=2)
-    elif mode == 5:
-        # rotate 180 degree and flip
-        out = np.rot90(out, k=2)
-        out = np.flipud(out)
-    elif mode == 6:
-        # rotate 270 degree
+    elif mode == 3: # rotate 270 degree
         out = np.rot90(out, k=3)
-    elif mode == 7:
-        # rotate 270 degree and flip
-        out = np.rot90(out, k=3)
-        out = np.flipud(out)
     return np.transpose(out, (2,0,1))
 
-def load_img(filepath):
-    img = cv2.imread(filepath,1)
-    img = img.astype(np.float32)
-    img = img/255.
-    img = np.transpose(img,(2,0,1))
-    return torch.Tensor(img).cuda()
