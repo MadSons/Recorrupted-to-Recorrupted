a812f6855af86915efd6cfa48e6a5bac71ea19a5
MadSons <madsenc004@gmail.com>
2024-03-06 21:34:41 -0500

    prepare data finished

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 
 class DnCNN(nn.Module):
-    def __init__(self, channels, num_of_layers=17, kernel_size = 3, padding = 1, features = 64):
+    def __init__(self, channels, num_of_layers=17, kernel_size=3, padding=1, features=64):
         super(DnCNN, self).__init__()
         layers = []
         layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
         layers.append(nn.ReLU(inplace=True))
-        for _ in range(num_of_layers-2):
+        for _ in range(num_of_layers - 2):
             layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
             layers.append(nn.BatchNorm2d(features))
             layers.append(nn.ReLU(inplace=True))
--- prepare_data.py
+++ prepare_data.py
@@ -1,20 +1,19 @@
 import os
 import numpy as np
 import random
-import torch
 import matplotlib.pyplot as plt
-import sys
-
 
 def noise(sigma, img):
     return img + np.random.normal(0, sigma / 255., img.shape)
 
 def create_patches(img, patch_size, stride):
+    
     patches = []
     for i in range(0, img.shape[0]-patch_size, stride):
         for j in range(0, img.shape[1]-patch_size, stride):
             patch = img[i:i+patch_size, j:j+patch_size]
             patches.append(patch)
+    patches = np.array(patches)
     return patches
 
 def augment(img):
@@ -48,13 +47,30 @@ if __name__ == '__main__':
 
     # 196 patches per image, 10 images
 
+    all_patches_aug_noisy = []
+    all_patches_aug = []
     for file in train_files:
         img = plt.imread(os.path.join('data/train', file))
         img_noise = noise(sigma, img)
         plt.imsave(os.path.join('data/train_noise', file), img_noise, cmap='gray')
 
+
         patches_aug_noisy = [augment(patch) for patch in create_patches(img_noise, patch, stride)]
         patches_aug = [augment(patch) for patch in create_patches(img, patch, stride)]
+
+        patches_aug_noisy = np.array(patches_aug_noisy)
+        patches_aug = np.array(patches_aug)
+
+        all_patches_aug_noisy.append(patches_aug_noisy)
+        all_patches_aug.append(patches_aug)
+
+    all_patches_aug_noisy = np.array(all_patches_aug_noisy)
+    all_patches_aug = np.array(all_patches_aug)
+
+    np.save('data/patches_aug_noisy.npy', all_patches_aug_noisy)
+    np.save('data/patches_aug.npy', all_patches_aug)
+
+    print(all_patches_aug_noisy.shape, all_patches_aug.shape)
 
     test_files = os.listdir('data/test')
     for file in test_files:
